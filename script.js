ac0a19739582fe24cfbb49ec0cb16830efb2c909
DaviMarques08 <davifelipebm1908@gmail.com>
2026-05-19 22:47:28 -0300

    Adiçao do botao para o usuario salvar na propria maquina

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const botaoCalcular = document.getElementById("btn-cal")
 const listaUl = document.getElementById("lista-produtos")
 const campoTotal = document.getElementById("total")
 const quantia = document.getElementById("qntd")
+const btnsalvar = document.getElementById("salvarL");
 
 let totalGeral = []
 totalGeral = JSON.parse(localStorage.getItem("minhaLista")) || []
@@ -76,6 +77,12 @@ function renderizarProduto(){
 
     campoTotal.innerText = `Total: R$ ${somaP.toFixed(2)}`
     localStorage.setItem("minhaLista", JSON.stringify(totalGeral));
+
+    if(totalGeral.length > 0) {
+        btnsalvar.style.display = "block"
+    } else {
+        btnsalvar.style.display = "none"
+    }
 }
 
 function limparCampos() {
@@ -84,5 +91,30 @@ function limparCampos() {
     quantia.value = ""
     inputMercadoria.focus()
 }
+
+
+    btnsalvar.addEventListener("click", () => {
+        if(totalGeral.length === 0) return;
+
+        let textoArquivo = "=== MINHA LISTA DE MERCADO ===\n\n"
+        let valorTotalGeral = 0;
+
+        totalGeral.forEach ((item) => {
+            const totalItem = item.preco * item.quantidade;
+            textoArquivo += `- ${item.produto} | Qtd: ${item.quantidade} | Preço Un: ${item.preco.toFixed(2)} | Total Item: R$ ${totalItem.toFixed(2)} \n`;
+            valorTotalGeral += totalItem;  
+        })
+
+        textoArquivo += `\n=============================\n`
+        textoArquivo += `TOTAL DA COMPRA: R$ ${valorTotalGeral.toFixed(2)}`
+
+        const blob = new Blob([textoArquivo], { type: "text/plain;charset=utf-8" });
+        const linkinvisivel = document.createElement("a");
+        linkinvisivel.href = URL.createObjectURL(blob);
+        linkinvisivel.download = "LISTA-DE-COMPRAS.txt";
+
+        linkinvisivel.click();
+        URL.revokeObjectURL(linkinvisivel.href);
+    })
 
 renderizarProduto()
